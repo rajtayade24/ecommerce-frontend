7be3c7de2c440c849db525ff7ecab60d67050dad
rajtayade24 <rajtayade2492006@gmail.com>
2026-01-05 19:29:24 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useEffect } from "react";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
 // import AdminRoutes from "@/routes/AdminRoutes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
